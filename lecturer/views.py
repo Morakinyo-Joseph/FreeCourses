145c2e0b5c9aff526d5f722009cbc2c961548064
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2022-06-12 08:01:50 +0100

    Updated dual branch

--- lecturer/views.py
+++ lecturer/views.py
@@ -2,53 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from .models import Course, Lecturer
 from .forms import CourseForm
 from django.contrib import messages
-<<<<<<< HEAD
-
-User = get_user_model()  #This isn't needed since User is imported directly
-
-# I changed from the generic class views to functions
-# the class views were left commented
-
-
-def signup(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-
-        if password1 == password2:
-            if User.objects.filter(username=username).exists():
-                messages.info(request, 'Username already exists!')
-                return redirect('signup')
-
-            elif User.objects.filter(email=email).exists():
-                messages.info(request, 'This email has already being used!')
-                return redirect('signup')
-
-            else:
-                new_user = User.objects.create_user(username=username, email=email, password=password1,
-                                                    first_name=first_name, last_name=last_name)
-                new_user.save()
-                return redirect('login')
-        else:
-            messages.info(request, "Passwords do not match")
-            return redirect('login')
-
-    else:
-        return render(request, "registration/signup.html")
-
-# class SignupView(generic.CreateView):
-#     template_name = "registration/signup.html"
-#     form_class = CustomUserCreationForm
-#
-#     def get_success_url(self):
-#         return reverse("login")
-=======
 from django.views import generic
->>>>>>> c97dd558a16ad29d17ae430f3e3d253d017f9629
 
 
 def course_list(request):
